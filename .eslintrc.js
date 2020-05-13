54071d18385004ee2dfb709a44f5ab633f5cb456
Tobias Laber <tobeno@gmx.de>
2020-05-14 01:20:03 +0200

    Add basic quality tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'local'],
   extends: [
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
     'prettier',
     'prettier/@typescript-eslint',
   ],
@@ -20,5 +22,37 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'no-public',
+      },
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'jest/consistent-test-it': [
+      'error',
+      {
+        fn: 'it',
+        withinDescribe: 'it',
+      },
+    ],
+    'jest/lowercase-name': [
+      'error',
+      {
+        ignore: ['describe'],
+      },
+    ],
+    'jest/no-alias-methods': 'error',
+    'jest/no-duplicate-hooks': 'error',
+    'jest/no-expect-resolves': 'error',
+    'jest/no-test-callback': 'off',
+    'jest/prefer-hooks-on-top': 'error',
   },
 };
