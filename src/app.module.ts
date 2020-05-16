0574ca60341e9c28b0ac3e1251637e81740db3d7
Tobias Laber <tobeno@gmx.de>
2020-05-17 00:07:34 +0200

    Add serializer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { DemoModule } from './modules/demo/demo.module';
 import { GlobalModule } from './modules/global/global.module';
 
 @Module({
   imports: [DemoModule, GlobalModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
