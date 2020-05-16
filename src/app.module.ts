612b0b2af135b6704078d736fe37830c48708cf6
Tobias Laber <tobeno@gmx.de>
2020-05-17 00:28:56 +0200

    Add HTTP config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import {
+  Module,
+  ClassSerializerInterceptor,
+  OnModuleInit,
+  HttpModule,
+  HttpService,
+} from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { DemoModule } from './modules/demo/demo.module';
+import { HTTP_CONFIG_DEFAULT } from './constants/http.constants';
 import { GlobalModule } from './modules/global/global.module';
 
 @Module({
-  imports: [DemoModule, GlobalModule],
+  imports: [DemoModule, GlobalModule, HttpModule],
   providers: [
     {
       provide: APP_INTERCEPTOR,
@@ -12,4 +19,14 @@ import { GlobalModule } from './modules/global/global.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly httpService: HttpService) {}
+
+  onModuleInit(): void {
+    // We don't use HttpModule.register to avoid registering the interceptors multiple times
+    this.httpService.axiosRef.defaults = {
+      ...this.httpService.axiosRef.defaults,
+      ...HTTP_CONFIG_DEFAULT,
+    };
+  }
+}
