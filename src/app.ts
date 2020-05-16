a01f8767567c8c799b1ef8e8dcca15f7ef9cbadb
Tobias Laber <tobeno@gmx.de>
2020-05-17 00:04:00 +0200

    Add validation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Express } from 'express';
-import { INestApplication } from '@nestjs/common';
+import {
+  INestApplication,
+  ValidationPipe,
+  BadRequestException,
+} from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { AppConfigService } from './modules/global/services/config/app-config.service';
+
+export function useValidation(app: INestApplication): void {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: errors => new BadRequestException(errors),
+    }),
+  );
+}
 
 export async function createApp(
   expressApp: Express,
@@ -13,6 +26,8 @@ export async function createApp(
     new ExpressAdapter(expressApp),
   );
   const appConfigService = app.get(AppConfigService);
+
+  useValidation(app);
 
   app.setGlobalPrefix(appConfigService.prefix);
 
